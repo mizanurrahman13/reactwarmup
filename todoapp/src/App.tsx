2c28455b10ee3a1693cbe3c9f9724622943e4fef
mizanurrahman13 <mizan.441980@gmail.com>
2024-12-24 07:15:45 +0600

    Added useState, form controlled, todoSummary, useEffect and custom hooks

--- todoapp/src/App.tsx
+++ todoapp/src/App.tsx
@@ -1,45 +1,35 @@
-import { useState } from "react"
-import TodoItem from "./components/todoitem"
-import { dummyData } from "./data/todos"
 import AddTodoFrom from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
+import TodoSummary from "./components/TodoSummary";
+import useTodos from "./hooks/useTodos";
 
 function App() {
-  const [todos, setTodos] = useState(dummyData)
-
-  function setTodoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodos) => 
-    prevTodos.map((todo) => (todo.id === id ? {...todo, completed} : todo))
-    );
-  }
-
-  function addTodo(title: string) {
-    setTodos(prevTodos => [
-      {
-        id: prevTodos.length + 1,
-        title,
-        completed: false
-      },
-      ...prevTodos
-    ])
-  }
+  const {
+    todos,
+    addTodo,
+    setTodoCompleted,
+    deleteTodo,
+    deleteAllCompletedTodos
+  } = useTodos();
 
   return (
-    <main className="py-10 h-screen space-y-5">
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Your Todos</h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
         <AddTodoFrom 
         onSubmit={addTodo}
         />
-        <div className="space-y-2">
-          {todos.map(todo => (
-            <TodoItem
-              key={todo.id}            
-              todo={todo}
-              onCompletedChange={setTodoCompleted}
-             />
-          ))}
-        </div>
+        <TodoList 
+        todos={todos}
+        onCompletedChange={setTodoCompleted}
+        onDelete={deleteTodo}
+        />
+        
       </div>
+      <TodoSummary 
+      todos={todos}
+      deleteAllCompleted={deleteAllCompletedTodos}
+      />
     </main>
   )
 }
